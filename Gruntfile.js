4af5c58194e9b110f1b9d582b2a3ceb34b435842
whoover <ugate.relay@gmail.com>
2014-02-26 13:55:55 -0500

    Release v1.0.0-beta.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,7 +169,7 @@ module.exports = function(grunt) {
 
 	// Test tasks
 	// TODO : move includes/copy
-	var buildTasks = [ /* 'clean', */, 'details', 'includes', 'copy:dist', 'uglify:js',
+	var buildTasks = [ /* 'clean', */, 'includes', 'copy:dist', 'uglify:js',
 			'uglify:docs', 'connect', 'qunit' ];
 	// Only run Sauce Labs tests if there's a Sauce access key
 	if (typeof process.env.SAUCE_ACCESS_KEY !== 'undefined' &&
