19922026018f4ba34575d3988cca79bf4201c9b7
whoover <ugate.relay@gmail.com>
2014-03-03 07:41:02 -0500

    Release v1.0.0-beta.4 [skip saucelabs-qunit]

--- grunt/tasks/release.js
+++ grunt/tasks/release.js
@@ -265,7 +265,7 @@ module.exports = function(grunt) {
 		fs.createReadStream(filePath, {
 			'flags' : 'r',
 			'encoding' : 'binary',
-			'mode' : '\0666',
+			'mode' : 438, /* 0666 */
 			'bufferSize' : chunkSize
 		}).addListener("data", function(chunk) {
 			// Since this is binary data, we cat use String.prototype.length We
